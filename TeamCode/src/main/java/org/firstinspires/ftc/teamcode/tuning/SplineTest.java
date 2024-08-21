b6f4a71cef1c9f28ad270203fcf6c9e4183bd7cb
jmez27 <jmesloh27@gmail.com>
2024-08-20 20:30:58 -0400

    Daniel Working on path finding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(-36, -58, Math.toRadians(180));
+        Pose2d beginPose = new Pose2d(-36, -58, Math.toRadians(90));
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -19,17 +19,33 @@ public final class SplineTest extends LinearOpMode {
 
             Actions.runBlocking(
                   drive.actionBuilder(beginPose)
-                          .splineToSplineHeading(new Pose2d(-55, -34, Math.toRadians(90)), Math.toRadians(90))
-                          .splineToSplineHeading(new Pose2d(-36, 0, Math.toRadians(0)), Math.toRadians(0))
-                          .lineToX(0)
-                          .setTangent(0)
-                          .splineToSplineHeading(new Pose2d(48, -36, Math.toRadians(90)), Math.toRadians(0))
                           .setTangent(Math.toRadians(90))
-                          .lineToY(47)
+                          .lineToY(-22)
+                          .turn(Math.toRadians(450))
+                          .setTangent(Math.toRadians(180))
+                          .splineToConstantHeading(new  Vector2d(-16, 10), Math.toRadians(135))
+                          .setTangent(Math.toRadians(135))
+                          .lineToX(-26)
+                          .turn(Math.toRadians(320))
+                          .turn(Math.toRadians(-50))
+                          .setTangent(Math.toRadians(270))
+                          .lineToY(56)
+                          .turn(Math.toRadians(-180))
                           .setTangent(Math.toRadians(0))
-                          .lineToXSplineHeading(0, Math.toRadians(180))
-                          .lineToX(-55)
+                          .lineToX(48)
+                          .setTangent(Math.toRadians(225))
+                          .lineToX(36)
+                          .setTangent(Math.toRadians(270))
+                          .lineToY(36)
+                          .turn(Math.toRadians(360))
+                          .lineToY(-12)
+                          .turn(Math.toRadians(360))
+                          .splineToLinearHeading(new Pose2d(22, -50, Math.toRadians(180)), Math.toRadians(-90))
+                          .setTangent(Math.toRadians(180))
+                          .lineToX(6)
+                          .turn(Math.toRadians(360))
                           .build());
+
 
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);
